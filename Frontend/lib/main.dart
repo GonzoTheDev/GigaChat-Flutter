3667e9737f0b40bbc706210071f3bb8c69ab34ac
GonzoTheDev <c20703429@mytudublin.ie>
2022-05-22 14:46:55 +0100

    Update main.dart

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
